c5421828fc2feaf2bc1679909fb483ff57eda1a1
Daniel Cook <cook200973@gmail.com>
2013-10-11 13:08:00 -0500

    Initial Commit

---
+++ Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Default" %>
+<asp:Content ID="Title" ContentPlaceHolderID="Title" runat="server">Home</asp:Content>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Navigation" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="PagePH" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="LeftColumnPH" Runat="Server">
+<dl>
+        <dt><asp:Label runat="server" ID="NameTBLabel" Text="User Name" AssociatedControlID="NameTB"></asp:Label></dt>
+        <dd><asp:TextBox runat="server" ID="NameTB" ></asp:TextBox></dd>
+        <dt><asp:Label runat="server" ID="PWTBLabel" Text="Password" AssociatedControlID="PWTB" ></asp:Label></dt>
+        <dd><asp:TextBox runat="server" ID="PWTB" TextMode="Password"></asp:TextBox></dd>        
+</dl>
+<asp:Button Text="Submit" ID="SubmitBtn" runat="server" OnClick="validate" />
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="SiteTitlePH" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="RightColumnPH" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content7" ContentPlaceHolderID="BottomRowPH" Runat="Server">
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void validate(object sender, EventArgs e)
+    {
+        string username = NameTB.Text;
+        string password = PWTB.Text;
+
+        using (SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["BarkeryCS"].ConnectionString))
+        using (SqlCommand sqlCmd = new SqlCommand("Select role FROM USERS WHERE username = @username AND password = @password", sqlCon))
+        {
+            sqlCmd.Parameters.Add("username", SqlDbType.VarChar).Value = username;
+            sqlCmd.Parameters.Add("password", SqlDbType.VarChar).Value = password;
+            sqlCon.Open();
+            sqlCmd.ExecuteNonQuery();
+            using (SqlDataReader SqlRdr = sqlCmd.ExecuteReader())
+            {
+                if (SqlRdr.Read())
+                {
+                    string role = SqlRdr["role"].ToString();
+                    if (role.Equals("admin"))
+                    {
+                        Response.Redirect("~/adminhome.aspx");
+                    }
+                    else
+                    {
+                        Response.Redirect("~/userhome.aspx");
+                    }
+                }
+                else
+                {
+                    Response.Redirect("~/loginerror.aspx");
+                }
+            }
+        }
+    }
+}
